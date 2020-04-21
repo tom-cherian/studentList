39866400b08bccdb50dc9192aacc5880076b2494
Tom Cherian <tom.cherian@experionglobal.local>
2020-04-21 11:48:52 +0530

    just Updated

--- src/components/editModal.js
+++ src/components/editModal.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const EditModal = (props) => {
